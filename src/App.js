19f90fb5c8248954b0326fa0563bb4e277433c68
Maksim Kramer <max.kramer@live.com>
2021-06-10 09:58:46 -0400

    first one with dangerousSetInnerHtml

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import marked from 'marked'
+import "./App.css";
+
+export default function App() {
+  const [markdown, setMarkdown] = useState("# sup");
+  function handleChange(e) {
+    setMarkdown(e.target.value);
+  }
+  return (
+    <div className='app'>
+      <textarea onChange={handleChange} value={markdown} />
+
+      <div className='preview' dangerouslySetInnerHTML={{__html: marked(markdown)}}/>
+    </div>
+  );
+}
